ae989d5963ed6ccf33f02c534dae5e2be1c9961f
Devendra Soni <devendrasoni984@gmail.com>
2023-01-10 21:03:39 +0530

    parentheis check

---
+++ Stacknew/index.js
@@ -0,0 +1,22 @@
+//{(({}))}
+
+const isValid = (str)=>{
+
+    const map={
+        '}' : '{',
+        ')' : '(',
+        ']' : '['
+    }
+    const arr=[]
+
+    for(const char of str){
+        if(!map[char]){
+            arr.push(char)
+        }
+        else if(arr.pop() !==map[char] ){
+            return false;
+        }
+    }
+    return arr.length === 0
+}
+console.log(isValid("(){{}}"))
