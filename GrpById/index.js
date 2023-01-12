b9f84f136fd4cb0613fdd351378316f0d6980f0a
Devendra Soni <devendrasoni984@gmail.com>
2023-01-12 23:41:21 +0530

    fix

---
+++ GrpById/index.js
@@ -0,0 +1,13 @@
+const products = [
+    { name: 'apples', category: 'fruits' },
+    { name: 'oranges', category: 'fruits' },
+    { name: 'potatoes', category: 'vegetables' }
+  ];
+
+  const groupByCategory = products.reduce((group, product) => {
+    const { category } = product;
+    group[category] = group[category] ?? [];
+    group[category].push(product);
+    return group;
+  }, {});
+  console.log(groupByCategory);
