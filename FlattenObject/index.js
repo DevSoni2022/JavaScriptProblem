11f179c7c83efbc19ff643564c64ab19972d296c
DevSoni2022 <121338813+DevSoni2022@users.noreply.github.com>
2024-11-16 21:51:23 +0530

    curry and flattern object

--- FlattenObject/index.js
+++ FlattenObject/index.js
@@ -1,1 +1,23 @@
-console.log("Test")
+let obj = {
+    a: 1,
+    b: {
+      c: {
+        d: 3,
+      },
+      e: 5,
+    },
+  }
+
+  function flattenObj(obj, parent = '', res = {}) {
+    for (const [key, value] of Object.entries(obj)) {
+        const propName = parent ? `${parent}.${key}` : key;
+        if (typeof value === 'object' && value !== null) {
+            flattenObj(value, propName, res);
+        } else {
+            res[propName] = value;
+        }
+    }
+    return res;
+}
+
+console.log(flattenObj(obj))
