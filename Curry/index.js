a094a10dfed3158b9390572ea7fccd99adc6e91b
DevSoni2022 <121338813+DevSoni2022@users.noreply.github.com>
2024-11-17 11:20:40 +0530

    curry

--- Curry/index.js
+++ Curry/index.js
@@ -1,3 +1,14 @@
 const sum =(a)=>(b)=>b? sum(a+b) : a;
 
 console.log(sum(2)(3)(4)())
+
+let sum2 = function (a) {
+    return function (b) {
+      if (b) {
+        return sum(a + b)
+      }
+      return a
+    }
+  }
+  
+  console.log(sum2(3)(5)(9)())
